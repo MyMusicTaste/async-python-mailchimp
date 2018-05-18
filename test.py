10a53dc6f1406fa23abca89da142a51e123dd966
MinJeong Kim <min7859@gmail.com>
2018-05-18 11:26:42 +0900

    <test> Add fail test and try/catch

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ You need to set `API_KEY`, `LIST_ID` ENV to successfully get result.
 import asyncio
 import os
 
+from mailchimp3.mailchimpclient import MailChimpError
+
 
 async def main():
     from mailchimp3 import MailChimp
@@ -14,8 +16,16 @@ async def main():
     LIST_ID = os.getenv('LIST_ID')
 
     client = MailChimp(API_KEY)
+
+    # SUCCESS
     result = await client.lists.get(LIST_ID)
     print(result)
+
+    # FAIL
+    try:
+        await client.lists.get('asdf')
+    except MailChimpError as e:
+        print(e)
 
 
 loop = asyncio.get_event_loop()
